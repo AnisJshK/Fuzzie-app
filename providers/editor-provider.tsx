a27381bd545a8a276023e0752a0a8abd66c8b112
AnisJshK <anisshaikh2131@gmail.com>
2026-04-17 10:43:36 +0530

    editor-node

--- providers/editor-provider.tsx
+++ providers/editor-provider.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { EditorActions, EditorNodeType } from '@/lib/types';
 import {
     Dispatch,
     createContext,
@@ -7,3 +8,96 @@ import {
     useEffect,
     useReducer
 } from 'react'
+
+
+export type EditorNode = EditorNodeType;
+
+export type Editor = {
+    elements : EditorNode[]
+    edges :{
+        id:string
+        source:string
+        target:string
+    }[]
+    selectedNode:EditorNodeType
+}
+
+export type HistoryState = {
+    history:Editor[]
+    currentIndex:number
+}
+export type EditorState = {
+    editor:Editor
+    history:HistoryState
+}
+
+const initialEditorState: EditorState['editor'] = {
+    elements:[],
+    selectedNode:{
+        data:{
+            completed:false,
+            current:false,
+            description:'',
+            metadata:{},
+            title:'',
+            type:'Trigger'
+        },
+        id:'',
+        position:{x:0,y:0},
+        type:'Trigger'
+    },
+    edges:[],
+}
+
+const initialHistoryState : HistoryState = {
+    history : [initialEditorState],
+    currentIndex:0,
+}
+
+const initialState: EditorState = {
+    editor:initialEditorState,
+    history:initialHistoryState
+}
+
+const editorReducer=(
+    state:EditorState = initialState,
+    action:EditorActions
+):EditorState => {
+
+    switch(action.type){
+        case 'REDO':
+            if(state.history.currentIndex < state.history.history.length-1){
+                const nextIndex = state.history.currentIndex-1
+                const nextEditorState = {...state.history.history[nextIndex]}
+                const redoState = {
+                    ...state,
+                    editor:nextEditorState,
+                    history:{
+                        ...state.history,
+                        currentIndex:nextIndex,
+                    },
+                }
+                return redoState
+            }
+            return state
+        
+        case 'UNDO':
+            if(state.history.currentIndex>0){
+                const prevIndex = state.history.currentIndex - 1
+                const prevEditorState = {...state.history.history[prevIndex]}
+                const undoState = {
+                    ...state,
+                    editor:prevEditorState,
+                    history:{
+                        ...state.history,
+                        currentIndex:prevIndex,
+                    },
+                }
+                return undoState
+            }
+            return state
+        default:
+            return state
+    }
+
+}
