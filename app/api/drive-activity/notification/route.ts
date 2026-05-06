160091403351739ea9bceb5e0abe1dc305982ddf
AnisJshK <anisshaikh2131@gmail.com>
2026-05-06 13:33:38 +0530

    added-notification-apis

---
+++ app/api/drive-activity/notification/route.ts
@@ -0,0 +1,74 @@
+import { postContentToWebHook } from "@/app/(main)/(pages)/connections/_actions/discord-connections";
+import { postMessageToSlack } from "@/app/(main)/(pages)/connections/_actions/slack-connection";
+import { db } from "@/lib/db";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function POST(req:NextRequest){
+    const headersList = headers()
+    let channelResourceId
+    ;(await headersList).forEach((value,key)=>{
+        if(key == 'x-goog-resource-id'){
+            channelResourceId = value
+        }
+    })
+    //WIP:Credits
+
+    if(channelResourceId){
+        const user = await db.user.findFirst({
+            where:{
+                googleResourceId:channelResourceId,
+            },
+            select:{
+                clerkId:true,credits:true
+            },
+        })
+        if((user && parseInt(user.credits!)>0)|| user?.credits=='Unlimited'){
+            const workflow = await db.workflows.findMany({
+                where:{
+                    userId:user.clerkId,
+                },
+
+            })
+            if(workflow){
+                workflow.map(async(flow)=>{
+                    const flowPath = JSON.parse(flow.flowPath!)
+                    let current = 0
+                    while(current<flowPath.length){
+                        if(flowPath[current]==='Discord'){
+                            const discordMessage = await db.discordWebhook.findFirst({
+                                where:{
+                                    userId:flow.userId,
+                                },
+                                select:{
+                                    url:true
+                                }
+                            })
+                            if(discordMessage){
+                                await postContentToWebHook(
+                                    flow.discordTemplate!,
+                                    discordMessage.url
+                                )
+                                flowPath.splice(flowPath[current],1)
+                            }
+                        }
+                        if(flowPath[current]==='Slack'){
+                            const channels = flow.slackChannels.map((channel)=>{
+                                return{
+                                    label:'',
+                                    value:channel,
+                                }
+                            })
+                            await postMessageToSlack(
+                                flow.slackAccessToken!,
+                                channels,
+                                flow.slackTemplate!
+                            )
+                            flowPath.splice(flowPath[current],1)
+                        }
+                    }
+                })
+            }
+        }
+    }
+}
